5befb732bd7640d15427870e586e63ca1edfc50c
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-03-18 00:11:12 +0200

    Animating colors

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include <math.h>
 #include "Screen.h"
 using namespace std;
 using namespace fgias;
@@ -14,14 +15,20 @@ int main() {
 
     while (true) {
         // draw particles
+        int elapsed = SDL_GetTicks();
+        unsigned char red = (1 + sin(elapsed * 0.001)) * 128; 
+        // declare it as `unsigned char` (cast) in order to discard decimal part 
+        // and restrict max value to 255
+
+        unsigned char green = (1 + sin(elapsed * 0.002)) * 128; 
+        unsigned char blue = (1 + sin(elapsed * 0.003)) * 128; 
+
         for (int y=0; y<Screen::SCREEN_HEIGHT; y++) {
             for (int x=0; x<Screen::SCREEN_WIDTH; x++) {
-                screen.setPixel(x, y, 128, 0, 255);
+                screen.setPixel(x, y, red, green, blue);
             }
         }
         
-        screen.setPixel(400, 300, 255, 255, 255);
-
         // update screen
         screen.update();
 
