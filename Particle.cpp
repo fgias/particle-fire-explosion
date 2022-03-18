138d41b29bb530529fecd861134816a2f0c89d85
Fotios Ioannis Giasemis <fotisfigz@gmail.com>
2022-03-19 00:44:49 +0200

    Tweak particle motion

--- Particle.cpp
+++ Particle.cpp
@@ -6,10 +6,14 @@ namespace fgias {
 
 Particle::Particle(): m_x(0), m_y(0) {
     m_direction = (2 * M_PI * rand())/RAND_MAX;
-    m_speed = (0.0003 * rand())/RAND_MAX;
+    m_speed = (0.02 * rand())/RAND_MAX;
+
+    m_speed *= m_speed;
 }
 
 void Particle::update(int interval) {
+
+    m_direction += interval * 0.0001;
 
     double xspeed = m_speed * cos(m_direction);
     double yspeed = m_speed * sin(m_direction);
